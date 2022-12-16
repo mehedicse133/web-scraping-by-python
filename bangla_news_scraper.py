7820c9fc41b90f83137230b162a162c9ed0a7ff4
mehedicse133 <mehedicse047@gmail.com>
2022-12-16 15:45:38 +0600

    add jugantor newscraper

--- bangla_news_scraper.py
+++ bangla_news_scraper.py
@@ -7,5 +7,58 @@ headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
     }
 
+# Jugantor Scraper
+def jugantor():
+    url = "https://www.jugantor.com/"
+
+    allnews = []
+
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    first_news = soup.find(
+        "div",
+        class_="card top_lead_card top_lead_card_type-2 border border-0 d-none d-sm-none d-md-block d-lg-block d-xl-block",
+    )
+
+    title =first_news.text.strip()
+    link = first_news.find("a")
+    link= link.get("href")
+
+    news = {"title": title, "link": link}
+    allnews.append(news)
+
+
+    second = soup.find_all(
+    "div", class_="col d-none d-sm-none d-md-none d-lg-block d-xl-block")
+    second = [i.find("a") for i in second]
+    # links = [i["href"] for i in second]
+    for t in second:
+        link = t["href"]
+        title = t.text.strip()
+        news = {"title": title, "link": link}
+        allnews.append(news)
+    
+
+    third = soup.find_all("div", class_="card top_lead_card d-md-none d-lg-none d-xl-none")
+    link = [i.find("a", href=True) for i in third]
+    for i in link:
+        link = i["href"]
+        title = i.text.strip()
+        news = {"title": title, "link": link}
+        allnews.append(news)
+
+
+    fourth = soup.find_all("div", class_="d-table-cell align-middle pr-2")
+    link = [i.find("a", href=True) for i in fourth]
+    for i in link[0:30]:
+        link = i["href"]
+        title = i.text.strip()
+        news = {"title": title, "link": link}
+        allnews.append(news)
+    print(allnews[:10])
+    return allnews    
+
+
+jugantor()   
 
 
