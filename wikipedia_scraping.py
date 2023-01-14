af31db98ccb4d8b6f2a9a4f56335f87a6e7ef0e6
mehedicse133 <mehedicse047@gmail.com>
2023-01-14 22:47:24 +0600

    create get_page method

--- wikipedia_scraping.py
+++ wikipedia_scraping.py
@@ -6,3 +6,10 @@ headers = {
     "User-Agernt": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 }
 
+url = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
+def get_page(url):   
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content,'html.parser')
+    print(soup)
+    return soup
+
