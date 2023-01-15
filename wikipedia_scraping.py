0b0ee227b2a04449bf7c9dacdbd5f025b0725e90
mehedicse133 <mehedicse047@gmail.com>
2023-01-15 12:03:35 +0600

    extract data table

--- wikipedia_scraping.py
+++ wikipedia_scraping.py
@@ -6,17 +6,28 @@ headers = {
     "User-Agernt": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 }
 
+
 url = 'https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population'
 def get_page(url):   
     r = requests.get(url)
     soup = BeautifulSoup(r.content,'html.parser')
-    print(soup)
     return soup
 
-def main():  
-    soup = get_page(url)
-
+def extract_data(soup):
+    table = soup.find('table',class_='wikitable sortable jquery-tablesorter')
+    
+    print(table)
 
 
 def save_data(data):
     pass
+
+def main():  
+    soup = get_page(url)
+   
+    data = extract_data(soup)
+    save_data(data)
+
+
+if __name__=="__main__":
+    main()
