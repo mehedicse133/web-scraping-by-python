c426dfdf57ff8a1afc69feee08f051b9e3da1a86
mehedicse133 <mehedicse047@gmail.com>
2023-02-04 22:09:05 +0600

    add get_html method

--- laliga_data_scsraping.py
+++ laliga_data_scsraping.py
@@ -3,11 +3,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def get_data():
+url = 'https://www.laliga.com/en-GB/laliga-santander/standing'
+
+def get_html(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
     }
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    html = soup.find_all(
+        'div', class_='styled__ContainerAccordion-e89col-11 HquGF')
 
+    return html
 
 
 
